0af2a57e733a32e5f0b089a2d7e2235ca3f415b5
Hannu Leinonen <hleinone@gmail.com>
2010-01-21 23:26:57 +0200

    Added Maven support for deploying 0.3.2 to a Maven repository.

---
+++ core/src/main/groovy/groovyx/gaelyk/GaelykBindingEnhancer.groovy
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package groovyx.gaelyk
+
+import com.google.appengine.api.datastore.DatastoreServiceFactory
+import com.google.appengine.api.images.ImagesServiceFactory
+import com.google.appengine.api.mail.MailServiceFactory
+import com.google.appengine.api.memcache.MemcacheServiceFactory
+import com.google.appengine.api.urlfetch.URLFetchServiceFactory
+import com.google.appengine.api.users.UserService
+import com.google.appengine.api.users.UserServiceFactory
+import com.google.appengine.api.labs.taskqueue.QueueFactory
+import com.google.appengine.api.xmpp.XMPPServiceFactory
+import com.google.apphosting.api.ApiProxy
+
+/**
+ * Class responsible for adding adding Google App Engine related services into the binding of Groovlets and Templates.
+ *
+ * @author Marcel Overdijk
+ * @author Guillaume Laforge
+ */
+class GaelykBindingEnhancer {
+
+    private Binding binding
+
+    GaelykBindingEnhancer(Binding binding) {
+        this.binding = binding
+    }
+
+    /**
+     * Bind the various Google App Engine services and variables
+     */
+    void bind() {
+        // bind google app engine services
+        binding.setVariable("datastore", DatastoreServiceFactory.datastoreService)
+        binding.setVariable("memcache", MemcacheServiceFactory.memcacheService)
+        binding.setVariable("urlFetch", URLFetchServiceFactory.URLFetchService)
+        binding.setVariable("mail", MailServiceFactory.mailService)
+        binding.setVariable("images", ImagesServiceFactory.imagesService)
+        
+        // bind user service and current user
+        UserService userService = UserServiceFactory.userService
+        binding.setVariable("users", userService)
+        binding.setVariable("user", userService.getCurrentUser())
+
+        // New in GAE SDK 1.2.5: task queues
+        binding.setVariable("defaultQueue", QueueFactory.getDefaultQueue())
+        binding.setVariable("queues", new QueueAccessor())
+
+        // New in GAE SDK 1.2.5: XMPP support
+        binding.setVariable("xmpp", XMPPServiceFactory.getXMPPService())
+
+        // Tells whether the application is running in local development mode
+        // or is deployed on Google's cloud
+        binding.setVariable("localMode", ApiProxy.currentEnvironment.class.name.contains("LocalHttpRequestEnvironment"))
+    }
+}
